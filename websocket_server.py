1eccab4312aaf3451403db69510220e4ba70719d
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-02-15 00:57:57 +0530

    Refactored websocket server code for improved readability and clarity

--- websocket_server.py
+++ websocket_server.py
@@ -1,18 +1,12 @@
 from websocket_server import WebsocketServer
-
-# Called for every client connecting (after handshake)
-def new_client(client, server):
-	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
-
-# Called when a client sends a message
+def new_client(client,server):
+    print("New client connected with id %d" %client['id'])
+    server.send_message('Hello Client!',client)
 def message_received(client, server, message):
-    print("Client(%d) said: %s" % (client['id'], message))
-    server.send_message_to_all(message)
-
-
+    print("Client(%d)sent:%s"%(client['id'], message))
+    server.send_message('You said:%s'%message,client)
 PORT = 9001
-server = WebsocketServer(port = PORT)
+server = WebsocketServer(port=PORT)
 server.set_fn_new_client(new_client)
-server.set_fn_message_received(message_received)
+server.set_fn_message_receive(message_received)
 server.run_forever()
